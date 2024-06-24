0b732320e8a58907a324e171b3783845e2e693df
shotofjin119 <shotofjin119@kakao.com>
2024-06-24 14:02:00 +0000

    kk

--- src/main/java/com/devdoc/backend/config/WebMvcConfig.java
+++ src/main/java/com/devdoc/backend/config/WebMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// WebMvcConfig: CORS 설정
+// WebMvcConfig: CORS 설정.
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
--- src/main/java/com/devdoc/backend/config/WebSecurityConfig.java
+++ src/main/java/com/devdoc/backend/config/WebSecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+// ..
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
