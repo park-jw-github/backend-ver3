747470a50d5a361b4d0770b7a61dc5c17744de3d
jw-park-github <jw.park.github@gmail.com>
2024-06-24 19:37:46 +0900

    heroku10

--- src/main/java/com/devdoc/backend/config/WebSecurityConfig.java
+++ src/main/java/com/devdoc/backend/config/WebSecurityConfig.java
@@ -31,12 +31,10 @@ public class WebSecurityConfig {
 				.cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
 				.csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
 				.httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 인증 비활성화
-				.sessionManagement(sessionManagement ->
-						sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용하지 않음 (Stateless)
-				.authorizeHttpRequests(authorize ->
-						authorize
-								.requestMatchers("/", "/auth/**", "/h2-console/**").permitAll() // 특정 경로 인증 없이 접근 허용
-								.anyRequest().authenticated()) // 나머지 모든 요청 인증 필요
+				.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용하지 않음 (Stateless)
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers("/", "/auth/**", "/h2-console/**").permitAll() // 특정 경로 인증 없이 접근 허용
+						.anyRequest().authenticated()) // 나머지 모든 요청 인증 필요
 				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); // h2-console 접근을 위한 설정. 같은 오리진에서의 프레임 사용 허용
 
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 인증 필터 추가
@@ -47,7 +45,11 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://devdoc-backend-a9c04f690ebd.herokuapp.com", "https://main.d3oxfc94kz63px.amplifyapp.com"));
+		configuration.setAllowedOrigins(Arrays.asList(
+				"http://localhost:3000",
+				"https://devdoc-backend-a9c04f690ebd.herokuapp.com",
+				"https://main.d3oxfc94kz63px.amplifyapp.com"
+		));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
@@ -58,5 +60,6 @@ public class WebSecurityConfig {
 		return source;
 	}
 }
+
 
 
